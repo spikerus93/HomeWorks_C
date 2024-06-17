290231a10ab4e8ab9efa9e00048fe78acf3eb448
Kirill Golubev <135273609+spikerus93@users.noreply.github.com>
2024-06-17 21:06:41 +0300

    Create hw1_task3.c
    Выполнение кода по заданию 3

---
+++ hw1_task3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  printf(" __    __\n");
+  printf("/  \\__/  \\ \n");
+  printf("\\__/  \\__/\n");
+  printf("/  \\__/  \\ \n");
+  printf("\\__/  \\__/\n");
+  return 0;
+}
